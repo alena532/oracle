6354ba0905d6ea1f758614e745fa029cd662a990
Alena Vorobey <yelena.vorobey.03@mail.ru>
2023-05-05 15:01:00 +0300

    add log tables

--- lab5/lab5.sql
+++ lab5/lab5.sql
@@ -22,3 +22,33 @@ CREATE TABLE STUDENTS
     group_id NUMBER        NOT NULL,
     CONSTRAINT fk_gr FOREIGN KEY (group_id) REFERENCES GROUPS (id) ON DELETE CASCADE
 );
+
+CREATE TABLE LOG_FACULTIES
+(
+    operation    VARCHAR(10) NOT NULL,
+    id           NUMBER,
+    old_name     VARCHAR(50),
+    new_name     VARCHAR(50),
+    date_founded DATE,
+    tm           TIMESTAMP   NOT NULL
+);
+
+CREATE TABLE LOG_GROUPS
+(
+    operation  VARCHAR(10) NOT NULL,
+    id         NUMBER,
+    old_name   VARCHAR(50),
+    new_name   VARCHAR(50),
+    faculty_id NUMBER,
+    tm         TIMESTAMP   NOT NULL
+);
+
+CREATE TABLE LOG_STUDENTS
+(
+    operation VARCHAR(10) NOT NULL,
+    id        NUMBER,
+    old_name  VARCHAR(50),
+    new_name  VARCHAR(50),
+    group_id  NUMBER,
+    tm        TIMESTAMP   NOT NULL
+);
